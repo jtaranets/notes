30325409e4b2f5b6475becf16e9e8a572665ff5a
Julia Taranets <jtaranets@gmail.com>
2019-09-21 11:52:04 +0300

    added route

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { saveNote, deleteNote } = require("./notes/createNote");
 
-router.post("/save-note", saveNote).get("/:id", deleteNote);
+router.post("/new-note", saveNote).get("/:id", deleteNote);
 
 module.exports = router;
