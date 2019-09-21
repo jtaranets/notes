81932e1356ab5344e23988f8ea51e977fad594f3
Julia Taranets <jtaranets@gmail.com>
2019-09-21 13:08:46 +0300

    added cors

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const app = require("./modules/app");
+const cors = require('cors')
 const express = require("express");
 const bodyParser = require("body-parser");
 const router = require("./routes/router");
 const startServer = port => {
   app.listen(port, () => {
+    app.use(cors());
     app.use(express.static("public/dist"));
     app.use(bodyParser.json());
     app.use("/", router);
